4af5d32068004f51119820fb03470179b4cd0530
JingJ <1337130115@qq.com>
2021-12-02 14:40:28 +0800

    购物车功能

---
+++ shop-cart/src/views/Carts.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(product,index) in products" :key="index">
+        {{product.name}}:{{product.quantity}}x{{product.price}}={{product.quantity*product.price}}
+      </li>
+    </ul>
+    <div>总价：{{totalPrice}}</div>
+  </div>
+</template>
+
+<script>
+import { mapState,mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapState("cart", {
+      products: "items",
+    }),
+    ...mapGetters('cart',['totalPrice'])
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ shop-cart/src/views/Products.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="(product,index) in products" :key="index">
+              {{product.name}}-{{product.price}}
+              <button @click="addToCart(product)" :disabled="product.inventory===0">放入购物车</button>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import {mapState,mapActions} from 'vuex'
+export default {
+    computed:{
+        ...mapState('product',{
+            products:'all'
+        }),
+    },
+    methods:{
+        ...mapActions('product',['loadData']),
+        ...mapActions('cart',['addToCart'])
+    },
+    mounted(){
+        this.loadData()
+    }
+}
+</script>
+
+<style>
+
+</style>
